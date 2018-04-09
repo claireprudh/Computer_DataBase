c0cbbdf3621c82615fb1edaec1bb256ad01e6a99
claireprudh <cprudhomme@excilys.com>
2018-04-09 11:59:50 +0200

    [WIP] Problèmes maven à soumettre à des personnes compétentes

---
+++ services/src/main/java/com/excilys/formation/cdb/spring/AppConfig.java
@@ -0,0 +1,26 @@
+
+package com.excilys.formation.cdb.spring;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+
+@Configuration
+@ComponentScan(basePackages = {"com.excilys.formation.cdb.pagination", 
+		"com.excilys.formation.cdb.services", 
+		"com.excilys.formation.cdb.servlet",
+		"com.excilys.formation.cdb.spring",
+		"com.excilys.formation.cdb.tag"})
+public class AppConfig {
+
+		@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("/WEB-INF/classes/messages");
+		return messageSource;
+	}
+	
+}
