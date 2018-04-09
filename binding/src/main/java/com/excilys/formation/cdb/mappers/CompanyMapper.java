617915e346886234a3133e5c449b242ba0093bd5
claireprudh <cprudhomme@excilys.com>
2018-04-09 15:11:08 +0200

    [WIP] Problèmes maven v2

--- binding/src/main/java/com/excilys/formation/cdb/mappers/CompanyMapper.java
+++ binding/src/main/java/com/excilys/formation/cdb/mappers/CompanyMapper.java
@@ -6,7 +6,7 @@ package com.excilys.formation.cdb.mappers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.springframework.jdbc.core.RowMapper;
+
 import org.springframework.stereotype.Component;
 
 import com.excilys.formation.cdb.dto.CompanyDTO;
@@ -18,17 +18,7 @@ import com.excilys.formation.cdb.persistence.Column;
  *
  */
 @Component
-public class CompanyMapper implements RowMapper<Company> {
-
-	public Company mapRow(ResultSet results, int arg1) throws SQLException {
-		
-		Company company = new Company();
-		company.setId(results.getInt(Column.CCID.getName()));
-		company.setName(results.getString(Column.CCNAME.getName()));
-		
-		return company;
-	
-	}
+public class CompanyMapper  {
 	
 	public CompanyDTO map(Company company) {
 		
