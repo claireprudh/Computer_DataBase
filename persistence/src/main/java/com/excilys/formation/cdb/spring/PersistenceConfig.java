cd1ec80e5cc92e4a274cf6c5813c8807f6ced02b
claireprudh <cprudhomme@excilys.com>
2018-04-11 17:00:21 +0200

    [FEAT] Maven Multimodules

---
+++ persistence/src/main/java/com/excilys/formation/cdb/spring/PersistenceConfig.java
@@ -0,0 +1,23 @@
+
+package com.excilys.formation.cdb.spring;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+
+@Configuration
+@ComponentScan(basePackages = {"com.excilys.formation.cdb.persistence", 
+		"com.excilys.formation.cdb.spring"})
+public class PersistenceConfig {
+
+		@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("/WEB-INF/classes/messages");
+		return messageSource;
+	}
+	
+}
---
+++ webapp/src/main/java/com/excilys/formation/cdb/spring/WebAppConfig.java
@@ -0,0 +1,25 @@
+
+package com.excilys.formation.cdb.spring;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+
+@Configuration
+@ComponentScan(basePackages = { 
+		"com.excilys.formation.cdb.servlet",
+		"com.excilys.formation.cdb.spring",
+		"com.excilys.formation.cdb.tag"})
+public class WebAppConfig {
+
+		@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("/WEB-INF/classes/messages");
+		return messageSource;
+	}
+	
+}
